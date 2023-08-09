00101a7b5a05a5e5c12d47b8ffc6c5091cb9eae3
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-08-08 19:12:48 -0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "../style/footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <section className="footer">
+        <p className="credits">
+          created by - <a href="https://github.com/RonyPadilla">RonyPadilla</a>{" "}
+          - devChallenges.io
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Footer 
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style/index.css'
+import Footer from './components/Footer.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Hola Mundo</h1>
+    <Footer/>
+  </React.StrictMode>,
+)
